be85daf128981dae437a32406834953dad5d629e
Vitalik-Hakim <jupiterxxx11@gmail.com>
2022-10-02 14:24:14 +0000

    minor change

--- pages/api/seed.js
+++ pages/api/seed.js
@@ -8,9 +8,9 @@ const handler = nc();
 
 handler.get(async (req, res) => {
   await db.connect();
-  await User.deleteMany();
-  await User.insertMany(data.users);
-  await Product.deleteMany();
+  // await User.deleteMany();
+  // await User.insertMany(data.users);
+  // await Product.deleteMany();
   await Product.insertMany(data.products);
   await db.disconnect();
   res.send({ message: 'seeded successfully' });
